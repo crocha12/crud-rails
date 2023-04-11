e55a224b735656b4970e0e440f3ceb192d576afb
Camilo Rocha <camilorocha11@gmail.com>
2023-04-11 11:11:02 -0300

    add validate times conflicts in create or update

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -23,13 +23,24 @@ class ActivitiesController < ApplicationController
   # POST /activities
   def create
     @activity = Activity.new(activity_params)
+    if verify_conflits
+      @activity.errors.add(:base, "There is a conflict with another activity")
+      return render :new, status: :unprocessable_entity, notice: "There is a conflict with another activity"
+    end
     return redirect_to activity_url(@activity), notice: "Activity was successfully created." if @activity.save 
     render :new, status: :unprocessable_entity
   end
 
   # PATCH/PUT /activities/1 or /activities/1.json
   def update
-    return redirect_to activity_url(@activity), notice: "Activity was successfully updated." if @activity.update(activity_params)
+    @activity.assign_attributes(activity_params)
+    if verify_conflits
+      @activity.errors.add(:base, "There is a conflict with another activity")
+      return render :edit, status: :unprocessable_entity, notice: "There is a conflict with another activity"
+    end
+    if @activity.save
+      return redirect_to activity_url(@activity), notice: "Activity was successfully updated."
+    end
     render :edit, status: :unprocessable_entity 
   end
 
@@ -48,5 +59,20 @@ class ActivitiesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def activity_params
       params.fetch(:activity).permit(:title, :description, :start_time, :end_time, :date)
+    end
+
+    def verify_conflits
+      @activities = Activity.where(date: @activity.date)
+      @activities.each do |activity|
+        next if activity.id == @activity.id
+        puts activity.start_time
+        puts activity.end_time
+        puts @activity.start_time
+        puts @activity.end_time
+        if @activity.start_time >= activity.start_time && @activity.start_time <= activity.end_time || @activity.end_time >= activity.start_time && @activity.end_time <= activity.end_time 
+          return true
+        end
+      end
+      return false
     end
 end
